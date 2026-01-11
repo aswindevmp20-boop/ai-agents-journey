d713901ebcc696ea38554a18dfd41cbba2afd4ba
aswindevmp20-boop <aswindevmp20@gmail.com>
2026-01-11 06:42:26 +0000

    Day 01 - AI Agent Fundamentals

---
+++ ai-agents-journey/day01_basic_agent/agent.py
@@ -0,0 +1,99 @@
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+from groq import Groq
+import os
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+# --- Step 1: Python tools ---
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+TOOL_FUNCTIONS = {
+    "add": add,
+    "subtract": subtract,
+}
+
+# --- Step 2: Tool schema ---
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "add",
+            "description": "Add two numbers",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "a": {"type": "number"},
+                    "b": {"type": "number"},
+                },
+                "required": ["a", "b"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "subtract",
+            "description": "Subtract b from a",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "a": {"type": "number"},
+                    "b": {"type": "number"},
+                },
+                "required": ["a", "b"],
+            },
+        },
+    },
+]
+
+# --- Step 3: Query ---
+query = (
+    "Task: Calculate 55 + 21, then subtract 10 from the result."
+    "You must complete the entire task using multiple tool calls if needed."
+    "After executing the first tool call, continue reasoning and call the next tool."
+    "Do not stop until the final result is reached."
+)
+
+# --- Step 4: Call Groq ---
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{"role": "user", "content": query}],
+    tools=tools,
+)
+
+print("\nRAW RESPONSE:\n", response, "\n")
+
+# --- Step 5: Handle tool calls ---
+msg = response.choices[0].message
+
+if msg.tool_calls:
+    for call in msg.tool_calls:
+
+        fn_name = call.function.name
+        raw_args = call.function.arguments
+
+        print("Raw args string:", raw_args)
+
+        try:
+            args = json.loads(raw_args)
+
+            # If model returns array instead of object
+            if isinstance(args, list) and len(args) == 1 and isinstance(args[0], dict):
+                args = args[0]
+
+        except json.JSONDecodeError:
+            raise ValueError("Model returned invalid JSON for tool arguments.")
+
+        print(f"Tool called: {fn_name}, args={args}")
+
+        # Execute tool
+        result = TOOL_FUNCTIONS[fn_name](**args)
+        print("Tool result:", result)
