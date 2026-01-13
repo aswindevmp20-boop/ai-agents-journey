73525b2824075b6951f9d6a95f1682caf8b4761f
aswindevmp20-boop <aswindevmp20@gmail.com>
2026-01-13 03:53:08 +0000

    Day 02 with expected output.

--- ai-agents-journey/day02_document_agent/agent.py
+++ ai-agents-journey/day02_document_agent/agent.py
@@ -86,3 +86,25 @@ if msg.tool_calls:
         )
 
         print("\n FINAL SUMMARY: \n", followup.choices[0].message.content)
+
+
+
+
+
+# Expected Output:-
+
+# RAW RESPONSE:
+#  ChatCompletion(id='chatcmpl-d9b5bcfa-28a5-4218-a470-028b7f34ede4', choices=[Choice(finish_reason='tool_calls', index=0, logprobs=None, message=ChatCompletionMessage(content=None, role='assistant', annotations=None, executed_tools=None, function_call=None, reasoning=None, tool_calls=[ChatCompletionMessageToolCall(id='wxmq92hsf', function=Function(arguments='{"path":"./sample.txt"}', name='read_file'), type='function')]))], created=1768276300, model='llama-3.3-70b-versatile', object='chat.completion', mcp_list_tools=None, service_tier='on_demand', system_fingerprint='fp_c06d5113ec', usage=CompletionUsage(completion_tokens=15, prompt_tokens=234, total_tokens=249, completion_time=0.047285142, completion_tokens_details=None, prompt_time=0.011670936, prompt_tokens_details=None, queue_time=0.058025477, total_time=0.058956078), usage_breakdown=None, x_groq=XGroq(id='req_01ketqp4e0fcc8bj9yv3w0s6jd', debug=None, seed=9638788, usage=None)) 
+
+# Raw args: {"path":"./sample.txt"}
+
+# Tool result (file content):
+#  Life in the small coastal town moved at a rhythm that felt almost separate from the rest of the world. Each morning, the gulls announced the sunrise long before the first boats drifted out of the harbor, their calls echoing across the water like impatient reminders of the day ahead. Locals wandered the cobblestone streets with a familiarity born from generations living in the same place, exchanging quiet nods or warm greetings as they passed. And though nothing remarkable ever seemed to happen there, the town held a quiet magic—one woven from salty air, worn wooden docks, and stories whispered between waves.
+
+#  FINAL SUMMARY: 
+#  Here are 4 bullet points summarizing the content of './sample.txt':
+
+# * The small coastal town has a unique rhythm that sets it apart from the rest of the world.
+# * The town's daily life begins with the sound of gulls announcing the sunrise, followed by the departure of boats from the harbor.
+# * The locals are deeply familiar with the town and its traditions, often greeting each other with nods or warm greetings as they pass by.
+# * Despite the lack of remarkable events, the town has a quiet magic that is woven from its natural surroundings, including the salty air, wooden docks, and the stories shared among its residents.
